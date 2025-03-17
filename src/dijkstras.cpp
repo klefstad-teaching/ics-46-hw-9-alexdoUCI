519e50685bb421bb9f2aa72495a1112c47e8c6f3
Alex Do <ado8@uci.edu>
2025-03-17 03:29:15 +0000

    shortest path done

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -2,7 +2,29 @@
 
 vector<int> dijkstra_shortest_path(const Graph& G, int souce, vector<int>& previous)
 {
-
+    int nv = G.size();
+    vector<int> distances(nv, INF);
+    vector<bool> visited(nv, false);
+    distances[source] = 0;
+    previous[source] = UNDEFINED;
+    priority_queue<pair<int, int>> min_heap;
+    min_heap.push({source, 0});
+    while(!min_heap.empty()) {
+        int u = min_heap.extractVertexWithMinimumWeight().first;
+        if (visited[u]) continue;
+        visited[u] = true;
+        for (Edge edge : G[u]) {
+            int v = edge.dst;
+            int w = edge.second;
+            if (!visited[v] && distances[u] + w < distances[v]) {
+                distances[v] = distances[u] + w;
+                previous[v] = u;
+                min_heap.push({v, distances[v]});
+            
+            }
+        }
+    }
+    return distances;
 }
 
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, comst vector<int>& previous, int destination)
