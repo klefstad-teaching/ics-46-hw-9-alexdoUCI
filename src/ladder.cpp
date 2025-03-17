3145d92d98964153ab592ba9c6fe13c1d41fdead
Alex Do <ado8@uci.edu>
2025-03-17 01:35:03 +0000

    i think i finished edit_distance_within. idk man someone send help

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -35,20 +35,39 @@ vector<string> generate_word_ladder(const string& begin_word, const string& word
 
 bool is_adjacent(const string& word1, const string& word2) // what the fuck even is this shit
 {
-    int len1 = word1.size();
-    int len2 = word2.size();
-    if (abs(len1 - len2) > 0) return false;
-    int diff_count = 0
-    for (i = 0, j = 0; i < len1 && j < len2; ) {
+    int word_size1 = word1.size();
+    int word_size2 = word2.size();
+    if (abs(word_size1 - word_size2) > 1) return false;
+    for (int i = 0, int j = 0, int diff_count = 0 ; i < word_size1 && j < word_size2; ) {
         if (word1[i] != word2[j]) return false;
-        if (len1 > len2) ++i;
-        else if (len1 < len2) ++j;
+        if (word_size1 > word_size2) ++i;
+        else if (word_size1 < word_size2) ++j;
         else {
             ++i;
             ++j;
         }
     }
     return true;
+}
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d)
+{
+    int str1_size = str1.size();
+    int str2_size = str2.size();
+    int diff_count = 0
+    if (std::abs(str1_size - str2_size) > d) return false;
+    for (int i = 0, int j = 0; i < str1_size && j < str2_size; ) {
+        if (str1_size[i] != str2_size[j]) {
+            if (++diff_count > d) return false;
+            if (str1_size > str2_size) ++i; // Skip a character in str1 (deletion)
+            else if (str1_size < str2_size) ++j; // Skip a character in str2 (insertion)
+            else { ++i; ++j; } // Substitution
+        } else {
+            ++i; ++j; // Characters match, move forward
+        }
+    }
+    diff_count += std::abs(str1_size - str2_size);
+    return diff_count <= d;
 }
 
 void load_words(set<string>& word_list, const string& file_name)
