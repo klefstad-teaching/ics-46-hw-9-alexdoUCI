b02029cf3fe0bcc4d7026a32bb2ba0c87c50267c
Alex Do <ado8@uci.edu>
2025-03-17 01:46:16 +0000

    verying wprd ladder done

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,4 +1,5 @@
 #include "ladder.h"
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
 
 void error(string word1, string word2, string msg)
 {
@@ -71,4 +72,15 @@ void print_word_ladder(const vector<string>& ladder)
 {
     for (word : ladder)
         cout << word << endl;
+}
+
+void verify_word_ladder() {
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
 }
